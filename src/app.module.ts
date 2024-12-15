75a3d9e453fb2a756a61625a6e0490fb7d752bf5
Woojin Lee <pappagallos@kakao.com>
2024-12-15 15:43:55 +0900

    추가한 Modules 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { PurchaseModule } from './purchase/purchase.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule,
+    ProductsModule,
+    PurchaseModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDevMode: boolean =
+    process.env.NODE_ENV === 'dev' ? true : false;
+
+  configure() {
+    // 개발자 모드로 실행 시 쿼리 logger가 출력되도록 실행
+    mongoose.set('debug', this.isDevMode);
+  }
+}
